93fa6d5f159b7b5f9fee5eeb75210511161d9755
nunopaiva1 <nunopaiva_1@hotmail.com>
2019-05-13 13:53:25 +0100

    Project IPVC

---
+++ Maintenance4.0/assets/Script/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        Destroy(gameObject);
+    }
+
+    public void TriggerDialogue_anim()
+    {
+        FindObjectOfType<DialogueManager_procedureAnim>().StartDialogue(dialogue);
+        Destroy(gameObject);
+    }
+
+}
